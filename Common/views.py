6af4276a2aa700a8a0525fe9f95fbec736a0c502
Krystian Baranowski <krystian.qaz123@gmail.com>
2019-03-24 14:41:47 +0100

    Remove ignored files

---
+++ Common/views.py
@@ -0,0 +1,104 @@
+from datetime import date
+from django.contrib import messages
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import EmailMessage
+from django.shortcuts import render, redirect
+from django.http import request, HttpResponse
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.views import generic, View
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth import login, update_session_auth_hash
+from django.contrib.auth.models import User
+
+from Clinic.models import User
+from Clinic.tokens import account_activation_token
+from Clinic.forms import UserForm
+
+
+# Create your views here.
+
+
+class RegisterView(View):
+    form_class = UserForm
+    template_name = 'register.html'
+
+    def get(self, request):
+        form = self.form_class(None)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            password_conf = form.cleaned_data['password_conf']
+            if password == password_conf:
+                user.set_password(password)
+                user.is_staff = True
+                user.is_active = False
+                user.save()
+                current_site = get_current_site(request)
+                mail_subject = 'Activate your clinic account.'
+                message = render_to_string('activate_email.html', {
+                    'user': user,
+                    'domain': current_site.domain,
+                    'uid': urlsafe_base64_encode(force_bytes(user.pk)).decode(),
+                    'token': account_activation_token.make_token(user),
+                })
+                to_email = form.cleaned_data.get('email')
+                email = EmailMessage(
+                    mail_subject, message, to=[to_email]
+                )
+                email.send()
+                messages.success(request, 'Please confirm your email address by '
+                                          'activation link to complete the registration')
+                return redirect('/')
+            else:
+                messages.error(request, 'Passwords did not match')
+        return render(request, self.template_name, {'form': form})
+
+
+class ActivateView(View):
+    template_name = 'base.html'
+
+    def get(self,request, uidb64, token):
+        try:
+            uid = force_text(urlsafe_base64_decode(uidb64))
+            user = User.objects.get(pk=uid)
+        except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+            messages.error(request, 'chujdupa')
+            return redirect('/')
+        if user is not None:
+            if account_activation_token.check_token(user, token):
+                user.is_active = True
+                user.save()
+                login(request, user)
+                messages.success(request, 'Email confirmed. You are logged in now.')
+                return redirect('/')
+        else:
+            messages.error(request, 'Activation link is invalid')
+            return redirect('/')
+        return render(request, self.template_name)
+
+
+class ChangePasswordView(View):
+    template_name = 'change_password.html'
+
+    def get(self, request):
+        form = PasswordChangeForm(user=request.user)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            # messages = ['Your password has been changed']
+            user.save()
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('/your_account')
+
+        return render(request, self.template_name, {'form': form})
---
+++ Project/urls.py
@@ -0,0 +1,47 @@
+"""Project URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.conf.urls import url
+from django.urls import path, include
+from Clinic.views import MainPage, DoctorsView, RegisterView, SetVisit, ContactView, \
+    ChangePasswordView, YourAccountView, ActivateView, EditProfileView, VisitsHistoryView, TreatmentHistoryView, \
+    SingleVisitView, DoctorInfoView
+from Project import settings
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('Common.urls')),
+    path('avatar/', include('avatar.urls')),
+    path('', MainPage.as_view(), name='main'),
+    path('doctors', DoctorsView.as_view(), name='doctors'),
+    path('doctor_info/<doctor_id>', DoctorInfoView.as_view(), name='doctor_info'),
+    path('set_visit', SetVisit.as_view(), name='set_visit'),
+    path('contact', ContactView.as_view(), name='contact'),
+    path('your_account', YourAccountView.as_view(), name='your_account'),
+    path('edit_profile', EditProfileView.as_view(), name='edit_profile'),
+    path('visits_history', VisitsHistoryView.as_view(), name='visits_history'),
+    path('visit/<visit_id>', SingleVisitView.as_view(), name='single_visit'),
+    path('treatment_history', TreatmentHistoryView.as_view(), name='results_view'),
+
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+

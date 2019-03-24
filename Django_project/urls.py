241c40294e8c8a40b473301ce63cc19a06275bb5
Krystian Baranowski <krystian.qaz123@gmail.com>
2019-03-24 13:46:17 +0100

    password resetting urls

--- Django_project/urls.py
+++ Django_project/urls.py
@@ -1,4 +1,4 @@
-"""Klinika URL Configuration
+"""Django_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
@@ -20,7 +20,7 @@ from django.urls import path, include
 from Clinic.views import MainPage, DoctorsView, RegisterView, SetVisit, ContactView, \
     ChangePasswordView, YourAccountView, ActivateView, EditProfileView, VisitsHistoryView, TreatmentHistoryView, \
     SingleVisitView, DoctorInfoView
-from Klinika import settings
+from Django_project import settings
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -30,13 +30,16 @@ urlpatterns = [
     path('', MainPage.as_view(), name='main'),
     path('doctors', DoctorsView.as_view(), name='doctors'),
     path('doctor_info/<doctor_id>', DoctorInfoView.as_view(), name='doctor_info'),
-    path('login/',
-         auth_views.LoginView.as_view(template_name='login.html'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'),
          name='login'),
-    path('password-reset/',
-         auth_views.PasswordResetView.as_view(
-             template_name='password_reset.html'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='password_reset.html'),
          name='password-reset'),
+    path('password-reset-confirm/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset.html'),
+         name='password_reset_confirm'),
+    path('password-reset-done', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'),
+         name='password_reset_done'),
+    path('password-reset-complete', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),
+         name='password_reset_complete'),
     path('logout/',
          auth_views.LogoutView.as_view(template_name='logout.html'),
          name='logout'),

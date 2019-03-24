cbd0ce4253a65a787dcf549c4ef7fdac2ca0f772
Krystian Baranowski <krystian.qaz123@gmail.com>
2019-03-24 17:19:39 +0100

    pointfield add

--- Clinic/admin.py
+++ Clinic/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
+from mapwidgets.widgets import GoogleStaticMapWidget, GoogleStaticOverlayMapWidget, GooglePointFieldWidget
+from django.contrib.gis.db import models
 
 # Register your models here.
-from .models import Visits, Results, Patient, Doctors
+from .models import Visits, Results, Patient, Doctors, Facility
 
 admin.site.register(Visits)
 admin.site.register(Results)
 admin.site.register(Patient)
 admin.site.register(Doctors)
+admin.site.register(Facility)
+
+
+class FacilityAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.PointField: {"widget": GoogleStaticMapWidget}
+    }
